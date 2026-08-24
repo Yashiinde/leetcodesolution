58824a3940e37d37748852f6281ff03f328ed5b1
Yashiinde <yashrajshinde056@gmail.com>
2026-08-24 22:32:29 +0530

    Time: 4 ms (96.24%), Space: 44.9 MB (50.25%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st=new Stack<>();
+        String[] parts=path.split("/");
+        for(String part: parts){
+            if(part.equals("") || part.equals(".")){
+                continue;
+            }else if(part.equals("..")){
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }
+            else{
+                st.push(part);
+            }
+        }
+        StringBuilder str=new StringBuilder("");
+
+        for(String dir:st){
+            str.append("/").append(dir);
+        }
+        if(str.length()==0){
+            return "/";
+        }
+        return str.toString();
+    }
+}
