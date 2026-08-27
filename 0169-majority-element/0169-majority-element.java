97fd9b8a04f8a080344bbc65945934407e7ba1dc
Yashiinde <yashrajshinde056@gmail.com>
2026-08-27 18:30:15 +0530

    Time: 12 ms (37.62%), Space: 52.9 MB (73.35%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,15 +4,11 @@ class Solution {
       HashMap<Integer,Integer> keys=new HashMap<>();
       for(int i=0;i<nums.length;i++){
         int num=nums[i];
-        if(keys.containsKey(num)){
-            keys.put(num,(keys.get(num))+1);
-        }else{
-            keys.put(num,1);
-        }
+        keys.put(num,keys.getOrDefault(num,0)+1);
+    
       } 
-    Set<Integer> keyset=keys.keySet(); 
       int majority=0;
-      for(Integer key:keyset){
+      for(Integer key:keys.keySet()){
         if(keys.get(key)>n){
             majority=key;
             break;
